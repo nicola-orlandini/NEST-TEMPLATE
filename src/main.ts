4bbed97b579ea1671f2c62cecaa7b0cd9c0b4fbc
Nicola Orlandini <nicolaorlandini96@gmail.com>
2024-04-26 21:07:39 +0200

    fix env var host

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,6 @@ async function bootstrap() {
     type: VersioningType.URI,
     defaultVersion: '1'
   });
-  await app.listen((parseInt(process.env.API_PORT) || 3000, (process.env.API_HOST) || 'localhost'));
+  await app.listen((parseInt(process.env.API_PORT)) || 3000, (process.env.API_HOST) || '127.0.0.1');
 }
 bootstrap();
